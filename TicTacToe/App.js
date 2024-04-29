8c6f8545d564d0513dd50ac79b9013dcbc91d61e
Nihal00 <nihallsuvarna@gmail.com>
2024-04-29 23:07:59 +0530

    Tic Tac Toe setup

---
+++ TicTacToe/App.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+
+      <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
